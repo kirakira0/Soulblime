2e734af0b8531e86cbe1c30ab8e3206207a116a1
kirakira0 <kiratoal0@gmail.com>
2019-11-01 15:04:02 -0700

    set up fg and bg

--- Assets/Scripts/GetPlayerInfo.cs
+++ Assets/Scripts/GetPlayerInfo.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class GetPlayerInfo : MonoBehaviour
 {
+    public InputField inputField; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //inputField = this.GetComponent<InputField>(); 
+        inputField.characterLimit = 12; 
     }
 
     // Update is called once per frame
